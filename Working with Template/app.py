bb0b207cc08a03e7efd74ec9c31598e5a25796c4
popeye98 <mhkartikey6101998@gmail.com>
2020-04-05 12:30:49 +0530

    add template func

---
+++ Working with Template/app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,redirect
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
+if __name__=='__main__':
+    app.run(debug=True) 
